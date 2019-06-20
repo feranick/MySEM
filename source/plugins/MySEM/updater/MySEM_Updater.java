98db570b2f492cdb8c2c4044b477f17fbea69cd8
Nicola Ferralis <feranick@hotmail.com>
2019-06-20 16:27:26 -0400

    Rev 3.1

--- source/plugins/MySEM/updater/MySEM_Updater.java
+++ source/plugins/MySEM/updater/MySEM_Updater.java
@@ -19,7 +19,8 @@ public class MySEM_Updater implements PlugIn {
 	private static boolean FirstRun = false; // false for upgrade, true for first run
 	private static boolean Uninstall = false; // false for upgrade, true for first run
 	public String currentVersion = "4.3";
-	public String url="http://github.com/feranick/MySEM/raw/master/source/";
+	//public String url="https://github.com/feranick/MySEM/raw/master/source/";
+    public String url="https://raw.githubusercontent.com/feranick/MySEM/master/source/";
 
 	public void run(String arg) {	
 		Cancel = false;
